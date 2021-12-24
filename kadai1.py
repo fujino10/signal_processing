effafd721399f8c33480ec4c793e48429ba33221
fujino10 <s20c1105py@s.chibakoudai.jp>
2021-12-24 15:21:04 +0900

    Add place of matplotlib

--- kadai1.py
+++ kadai1.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from  scipy.io.wavfile  import read
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# matplotlibの設定
+fig = plt.figure(figsize = (8, 8))
+
+ax = fig.add_subplot(111, projection='3d')
+
+ax.set_title("fft of wave", size = 20)
+
+ax.set_xlabel("x", size = 14)
+ax.set_ylabel("y", size = 14)
+ax.set_zlabel("z", size = 14)
+
+wavefile = "./test.wav"
+
+fs, data = read(wavefile)
+
+print "Sampling rate :", fs
+
+if (data.shape[1] == 2):
+    left = data[:, 0]
+    right = data[:, 1]
 
 
 
